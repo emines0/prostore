88bfbd426179a9d3f428d0e3e591e33ea5566607
Emin Sefa <emin.sefa1@gmail.com>
2025-12-19 15:53:17 +0100

    Header and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import "@/assets/styles/globals.css"
+import Header from "@/components/shared/header"
+import Footer from "@/components/footer"
 
 export const metadata: Metadata = {
   title: "Prostore",
@@ -13,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   )
 }
